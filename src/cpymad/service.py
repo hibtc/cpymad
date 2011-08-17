cca73aa0fda6747171a9ce0b85e13a46c2e376de
ylevinse <ylevinse@e02d3557-2f25-4247-9e18-1207d715a173>
2011-08-17 07:29:24 +0000

    added cpymad service
    git-svn-id: svn+ssh://cernsvn/reps/acc-co/trunk/accmodel/pymad/pymad@200649 e02d3557-2f25-4247-9e18-1207d715a173

--- src/cpymad/service.py
+++ src/cpymad/service.py
@@ -4,6 +4,18 @@ Created on 16 Aug 2011
 @author: kfuchsbe
 '''
 from pymad.abc.service import PyMadService
-
-class CPyMadService(PyMadService):
-    pass
+import cpymad,madx
+class CpyMadService(PyMadService):
+    ''' The CPymad implementation of the
+        abstract class PyMadService. '''
+    
+    def mdefs(self):
+        return cpymad.modelList
+    
+    def models(self):
+        return madx.list_of_models
+    
+    def create_model(self, modeldef):
+        cpymad.model(modeldef)
+    
+>>>>>>> added cpymad service
