0228b6fcbc5d378e2baa6a9e6dff44af8652e32a
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-08-21 16:09:55 +0200

    moved all files into subfolder cern, imports not yet updated..

--- src/setup.py
+++ src/setup.py
@@ -21,16 +21,17 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-sourcefiles=["cpymad/madx.pyx"] # this can be a list of both c and pyx source files..
-pythonsrc=["cpymad",            # list of python modules to include
-           "cpymad._models",
-           "jpymad",
-           "jpymad.tools", 
-           "pymad",
-           "pymad.io",
-           "pymad.abc",
-           "pymad.tools",
-           "pymad.domain"] 
+sourcefiles=["cern/cpymad/madx.pyx"] # this can be a list of both c and pyx source files..
+pythonsrc=["cern",
+           "cern.cpymad",
+           "cern.cpymad._models",
+           "cern.jpymad",
+           "cern.jpymad.tools", 
+           "cern.pymad",
+           "cern.pymad.io",
+           "cern.pymad.abc",
+           "cern.pymad.tools",
+           "cern.pymad.domain"] 
 cdata=['_models/*.json','_models/*.madx'] # list of data files to include..
 libs=['madx', "X11", "z", "pthread", "c", "stdc++"]
 includedirs=['/usr/local/include/madX',
@@ -38,7 +39,7 @@ includedirs=['/usr/local/include/madX',
              '/afs/cern.ch/user/y/ylevinse/.local/include/madX']
 libdirs=['/afs/cern.ch/user/y/ylevinse/.local/lib']
 
-madmodule=Extension('madx',
+madmodule=Extension('cern.madx',
                     define_macros = [('MAJOR_VERSION', '0'),
                                      ('MINOR_VERSION', '1')],
                     include_dirs = includedirs,
@@ -58,5 +59,5 @@ setup(
     license = 'CERN Standard Copyright License',
     requires=['Cython','numpy','Py4J'],
     packages = pythonsrc,
-    package_data={'cpymad': cdata},
+    package_data={'cern.cpymad': cdata},
     )
