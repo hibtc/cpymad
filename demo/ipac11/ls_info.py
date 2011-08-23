77bf782fd48e3f6c003df779f23e620f9d6c192f
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-08-23 15:44:37 +0200

    iterated plots..
    I had introduced a bug in ls.py

--- demo/ipac11/ls_info.py
+++ demo/ipac11/ls_info.py
@@ -3,10 +3,11 @@ Created on Aug 20, 2011
 
 @author: kaifox
 '''
-import pymad as pm
+from cern import pymad as pm
 
 #pm.init('cpymad')
-pm.init('jpymad', start='gui')
+#pm.init('jpymad', start='gui')
+pms=pm.init('jpymad', start='service')
 
 # print the name of all model definitions
 pm.ls_mdefs()
