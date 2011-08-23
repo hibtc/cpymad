5a12b67ea1dc23c6f5e32c526d99f83b26c90b50
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-08-23 15:44:37 +0200

    iterated plots..

--- demo/ipac11/plot_beta.py
+++ demo/ipac11/plot_beta.py
@@ -4,8 +4,10 @@ from matplotlib import pyplot as plt
 from cern import pymad as pm
 
 # create the service
-pms = pm.init('cpymad')
+#pms = pm.init('cpymad')
 #pms = pm.init('jpymad', start='gui')
+pms = pm.init('jpymad', start='service')
+#pms = pm.init('jpymad')
 
 # print the name of all model definitions
 print pms.mdefnames
@@ -13,9 +15,9 @@ print pms.mdefnames
 # get one model-definition
 model = pms.create_model('lhc')
 
-## obtain get twiss table in a python object
+# obtain get twiss table in a python object
 table,summary=model.twiss(columns=['name', 's', 'betx', 'bety'])
 
-## plot the result
+# plot the result
 plt.plot(table.s,table.betx)
 plt.show()
