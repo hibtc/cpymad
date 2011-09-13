4ec956e420f943d0f196dd1547d5b68826815292
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-09-13 11:05:15 +0200

    fixed setup.py to include tfs data files
    M	src/setup.py

--- src/setup.py
+++ src/setup.py
@@ -36,24 +36,10 @@ cdata=['_models/*.json',
        '_models/*.madx',
        'jmad_models_json/*.cpymad.json'] # list of data files to include..
 # add this to include data array
-redata=[
-         '_models/re*data/*/*.madx',
-         '_models/re*data/*/*.str',
-         '_models/re*data/*/*.seq',
-         '_models/re*data/*/*/*.madx',
-         '_models/re*data/*/*/*.str',
-         '_models/re*data/*/*/*.seq',
-         '_models/re*data/*/*/*/*.madx',
-         '_models/re*data/*/*/*/*.str',
-         '_models/re*data/*/*/*/*.seq',
-         '_models/re*data/*/*/*/*/*.madx',
-         '_models/re*data/*/*/*/*/*.str',
-         '_models/re*data/*/*/*/*/*.seq',
-         '_models/re*data/*/*/*/*/*/*.madx',
-         '_models/re*data/*/*/*/*/*/*.str',
-         '_models/re*data/*/*/*/*/*/*.seq',
-         ]
-cdata.extend(redata)
+for j in range(2,10):
+    for end in ['.madx','.str','.seq','.tfs']:
+        cdata.append('_models/re*data'+'/*'*j+end)
+
 libs=['madx', "X11", "z", "pthread", "c", "stdc++"]
 
 def add_dir(directory,dirlist):
