539d00cf4a483d6c68b6d1efab47e0618601b49d
Thomas Gläßle <t_glaessle@gmx.de>
2013-06-23 06:20:43 +0200

    adding libmadx._madx_tools._mad_command

--- src/cern/libmadx/_madx_tools.py
+++ src/cern/libmadx/_madx_tools.py
@@ -45,3 +45,42 @@ def _add_offsets(offsets):
     if offsets:
         return 'offsetelem="'+offsets+'",'
     return ''
+
+def _mad_command(cmd, *args, **kwargs):
+    """
+    Create a MAD-X command from its name and parameter list.
+    
+    @param cmd [string] name of the MAD command
+    @params *args [list] ordered arguments to the MAD command
+    @params **kwargs [dict] unordered arguments to the MAD command
+
+    Examples:
+
+    >>> _mad_command('twiss', ('sequence', lhc), 'centre', dx=2, betx=3, bety=8)
+    twiss, sequence=lhc, centre, betx=3, bety=3, dx=2;
+
+    >>> _mad_command('option', echo=False)
+    option, -echo;
+
+    Note that alphabetic order is enforced on kwargs, such that results are
+    always reproducible.
+
+    """
+    mad = cmd
+    fullargs = list(args) + sorted(kwargs.items(), key=lambda i: i[0])
+    for arg in fullargs:
+        try:
+            key, value = arg
+        except ValueError:
+            key = arg
+            value = True
+        key = str(key)
+        if key.lower() == 'range':
+            mad += ', ' + key + '=' + _add_range(value)[:-1]
+        elif isinstance(value, bool):
+            mad += ', ' + ('' if value else '-') + key
+        else:
+            mad += ', ' + key + '=' + str(value)
+    mad += ';'
+    return mad
+
