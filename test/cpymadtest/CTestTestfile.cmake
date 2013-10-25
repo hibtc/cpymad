4d0a5a9404b8fe0c9a4c6fb873712ea55fb8b986
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2013-10-25 18:01:29 +0200

    tests returns 0 when failing

--- test/cpymadtest/CTestTestfile.cmake
+++ test/cpymadtest/CTestTestfile.cmake
@@ -7,5 +7,5 @@ endforeach()
 # 'ctest -LE SLOW'
 foreach(PYTEST lhc-b4 lhc)
    add_test(test_${PYTEST} ${PYTHON} test_${PYTEST}.py)
-   set_tests_properties(test_${PYTEST} PROPERTIES LABELS SLOW)
+   set_tests_properties(test_${PYTEST} PROPERTIES LABELS SLOW FAIL_REGULAR_EXPRESSION "FAILED")
 endforeach()
