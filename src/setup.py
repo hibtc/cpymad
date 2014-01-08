61872b9bc9ead91cb70f7c1d0445b09da6c4eed5
Thomas Gläßle <t_glaessle@gmx.de>
2014-01-08 04:36:39 +0100

    Cleanup setup.py
    Remove some obsolete code and redundancies.

--- src/setup.py
+++ src/setup.py
@@ -15,150 +15,122 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #-------------------------------------------------------------------------------
-#!/usr/bin/python
 
-import os,sys
+import sys
+from os import path
 
 # Version of pymad (major,minor):
 PYMADVERSION=['0','4']
 
-# With Python 2.6, the cythoning does not work
-# with setuptools:
+# With Python 2.6, the cythoning does not work with setuptools:
 if sys.version_info < (2,7):
     from distutils.core import setup
     from distutils.extension import Extension
+    extra_setup_args = {}
 else: # should be default asap
     from setuptools import setup
     from setuptools.extension import Extension
+    extra_setup_args = dict(
+        setup_requires=['numpy', 'Cython'],
+        install_requires=['numpy'])
 
 from Cython.Distutils import build_ext
 import platform
 from distutils.util import get_platform
 import numpy
 
-# ugly hack to add --madxdir=/path/to/madxinstallation
-special_madxdir=''
-for arg in sys.argv:
-    if '--madxdir=' in arg:
-        special_madxdir=arg.split('=')[1]
+# parse command line option: --madxdir=/path/to/madxinstallation
+special_madxdir = ''
+for arg in list(sys.argv):  # avoid problems due to side-effects by copying sys.argv into a temporary list
+    if arg.startswith('--madxdir='):
+        special_madxdir = arg.split('=', maxsplit=1)[1]
         sys.argv.remove(arg)
 
+def add_dir(dirlist, directory):
+    if path.isdir(directory) and directory not in dirlist:
+        dirlist.append(directory)
 
-sourcefiles=[["cern/madx.pyx"],["cern/libmadx/table.pyx"]]
-pythonsrc=["cern",
-           "cern.libmadx",
-           "cern.resource",
-           "cern.cpymad",
-           "cern.cpymad._couch",
-           "cern.jpymad",
-           "cern.jpymad.tools",
-           "cern.pymad",
-           "cern.pymad.io",
-           "cern.pymad.abc",
-           "cern.pymad.tools",
-           "cern.pymad.domain"]
+# guess prefixes for possible header/library locations
+if special_madxdir:
+    _prefixdirs = [special_madxdir]
+else:
+    _prefixdirs = [sys.prefix]
+add_dir(_prefixdirs, '/usr')
+add_dir(_prefixdirs, '/usr/local')
+add_dir(_prefixdirs, path.join(path.expanduser('~'),'.local'))
 
-# list of data files to include..
-cdata=['_models/*.json']
+# extra include pathes: madx
+includedirs = [path.join(d, 'include')
+               for d in _prefixdirs
+               if path.isdir(path.join(d, 'include', 'madX'))]
+if not includedirs:
+    raise RuntimeError("Cannot find folder with Mad-X headers")
 
-# add this to include data array
-for j in range(2,10):
-    for end in ['.madx','.str','.seq','.tfs', '.xsifx', 'CLICx' ,'.ind92']:
-        cdata.append('_models/re*data'+'/*'*j+end)
+# Add numpy include directory (for cern.libmadx.table):
+includedirs.append(numpy.get_include())
 
-libs=['madx', 'stdc++']
+# static library pathes
+libdirs = []        # static library pathes
+for prefixdir in _prefixdirs:
+    add_dir(libdirs, path.join(prefixdir,'lib'))
+    if platform.architecture()[0]=='64bit':
+        add_dir(libdirs, path.join(prefixdir,'lib64'))
+
+# runtime library pathes
+rlibdirs = []
+for ldir in libdirs:
+    if any(path.isfile(path.join(ldir,'libmadx.'+suffix))
+           for suffix in ['so','dll','dylib']):
+        rlibdirs = [ldir]
+        libdirs = [ldir]    # overwrites libdirs, is this intentional?
+        break
+
+# required libraries
+libs = ['madx', 'stdc++']
 if get_platform() == "win32":
     libs += ['ptc', 'gfortran', 'msvcrt']
 else:
     libs += ['c']
 
-def add_dir(directory,dirlist):
-    if os.path.isdir(directory):
-        if directory not in dirlist:
-            dirlist.append(directory)
+# common cython arguments
+cython_args = dict(
+    define_macros=[('MAJOR_VERSION', PYMADVERSION[0]),
+                   ('MINOR_VERSION', PYMADVERSION[1])],
+    include_dirs=includedirs,
+    libraries=libs,
+    library_dirs=libdirs,
+    runtime_library_dirs=rlibdirs)
 
-includedirs=[]
-libdirs=[]
-rlibdirs=[]
-if special_madxdir:
-    _prefixdirs=[special_madxdir]
-else: # making some guesses...
-    _prefixdirs=[ sys.prefix, ]
+setup(
+    name='cern-pymad',
+    version='.'.join(map(str, PYMADVERSION)),
+    description='Interface to Mad-X, using Cython or Py4J through JMAD',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+        Extension('cern.madx',
+                  sources=["cern/madx.pyx"],
+                  **cython_args),
+        Extension('cern.libmadx.table',
+                  sources=["cern/libmadx/table.pyx"],
+                  **cython_args),
+    ],
+    packages = [
+        "cern",
+        "cern.libmadx",
+        "cern.resource",
+        "cern.cpymad",
+        "cern.cpymad._couch",
+        "cern.jpymad",
+        "cern.jpymad.tools",
+        "cern.pymad",
+        "cern.pymad.io",
+        "cern.pymad.abc",
+        "cern.pymad.tools",
+        "cern.pymad.domain"
+    ],
+    include_package_data=True, # include files matched by MANIFEST.in
+    author='PyMAD developers',
+    author_email='pymad@cern.ch',
+    license = 'CERN Standard Copyright License',
+    **extra_setup_args)
 
-for prefixdir in ['/usr',
-        '/usr/local',
-        os.path.join(os.path.expanduser('~'),'.local')]:
-    add_dir(prefixdir,_prefixdirs)
-
-for prefixdir in _prefixdirs:
-    if os.path.isdir(os.path.join(prefixdir,'include','madX')):
-        add_dir(os.path.join(prefixdir,'include'),includedirs)
-        break
-if not includedirs:
-    raise ValueError("Cannot find folder with Mad-X headers")
-
-# Add numpy include directory (for cern.libmadx.table):
-includedirs.append(numpy.get_include())
-
-for prefixdir in _prefixdirs:
-    add_dir(os.path.join(prefixdir,'lib'),libdirs)
-    if platform.architecture()[0]=='64bit':
-        add_dir(os.path.join(prefixdir,'lib64'),libdirs)
-
-for ldir in libdirs:
-    if rlibdirs:
-        break
-    for suffix in ['so','dll','dylib']:
-        if os.path.isfile(os.path.join(ldir,'libmadx.'+suffix)):
-            rlibdirs=[ldir]
-            libdirs=[ldir]
-            break
-mods=[Extension('cern.madx',
-        define_macros = [('MAJOR_VERSION', PYMADVERSION[0]),
-                            ('MINOR_VERSION', PYMADVERSION[1])],
-        include_dirs = includedirs,
-        libraries = libs,
-        sources = sourcefiles[0],
-        library_dirs = libdirs,
-        runtime_library_dirs= rlibdirs
-        ),
-      Extension('cern.libmadx.table',
-                    define_macros = [('MAJOR_VERSION', PYMADVERSION[0]),
-                                     ('MINOR_VERSION', PYMADVERSION[1])],
-                    include_dirs = includedirs,
-                    libraries = libs,
-                    sources = sourcefiles[1],
-                    library_dirs = libdirs,
-                    runtime_library_dirs= rlibdirs
-                    ),
-      ]
-
-if sys.version_info < (2,7):
-    # can be deleted when we don't support Python 2.6 anymore..
-    setup(
-        name='cern-pymad',
-        version='.'.join([str(i) for i in PYMADVERSION]),
-        description='Interface to Mad-X, using Cython or Py4J through JMAD',
-        cmdclass = {'build_ext': build_ext},
-        ext_modules = mods,
-        author='PyMAD developers',
-        author_email='pymad@cern.ch',
-        license = 'CERN Standard Copyright License',
-        packages = pythonsrc,
-        package_data={'cern.cpymad': cdata}
-        )
-else:
-    setup(
-        name='cern-pymad',
-        version='.'.join([str(i) for i in PYMADVERSION]),
-        description='Interface to Mad-X, using Cython or Py4J through JMAD',
-        cmdclass = {'build_ext': build_ext},
-        ext_modules = mods,
-        author='PyMAD developers',
-        author_email='pymad@cern.ch',
-        license = 'CERN Standard Copyright License',
-        packages = pythonsrc,
-        package_data={'cern.cpymad': cdata},
-        setup_requires=['numpy', 'Cython'],
-        install_requires=['numpy'],
-        )
