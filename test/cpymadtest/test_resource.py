1933aed205d63e1e63977f442f07080a86adbf69
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2014-01-20 14:32:19 +0100

    resource test converted from json to yaml

--- test/cpymadtest/test_resource.py
+++ test/cpymadtest/test_resource.py
@@ -19,7 +19,7 @@ from cern.resource.couch import CouchResource
 import unittest
 import tempfile
 import shutil
-import json
+import yaml
 import os.path
 import gc
 import sys
@@ -39,7 +39,7 @@ def create_test_file(base, path, content=None):
         pass
     with open(os.path.join(base, *path), 'wt', encoding='utf-8') as f:
         if content is None:
-            # With json.dump is not compatible in python2 and python3
+            # With yaml.dump is not compatible in python2 and python3
             f.write(u'{"path": "%s", "unicode": "%s"}' % (
                 os.path.join(*path),    # this content is predictable
                 u"äæo≤»で"))            # some unicode test data
@@ -76,65 +76,65 @@ class Common(object):
         self.base = tempfile.mkdtemp()
         self.path = os.path.join(self.base, self.mod)
         create_test_file(self.path, ['__init__.py'], u'')
-        create_test_file(self.path, ['a.json'])
-        create_test_file(self.path, ['subdir', 'b.json'])
+        create_test_file(self.path, ['a.yml'])
+        create_test_file(self.path, ['subdir', 'b.yml'])
 
     def tearDown(self):
         gc.collect()
         shutil.rmtree(self.base)
 
     def test_open(self):
-        with self.res.open('a.json', 'utf-8') as f:
+        with self.res.open('a.yml', 'utf-8') as f:
             self.assertEqual(
-                json.loads(f.read())['path'],
-                'a.json')
-        with self.res.get('subdir/b.json').open(encoding='utf-8') as f:
+                yaml.load(f.read())['path'],
+                'a.yml')
+        with self.res.get('subdir/b.yml').open(encoding='utf-8') as f:
             self.assertEqual(
-                    json.loads(f.read())['path'],
-                    os.path.join('subdir', 'b.json'))
+                    yaml.load(f.read())['path'],
+                    os.path.join('subdir', 'b.yml'))
 
     def test_list(self):
         self.assertEqual(
                 set_(self.res.listdir()),
-                set(('__init__.py', 'a.json', 'subdir')))
+                set(('__init__.py', 'a.yml', 'subdir')))
         self.assertEqual(
                 set(self.res.listdir('subdir')),
-                set(('b.json',)))
+                set(('b.yml',)))
         self.assertEqual(
                 set(self.res.get('subdir').listdir()),
-                set(('b.json',)))
+                set(('b.yml',)))
 
     def test_provider(self):
         self.assertEqual(
                 set_(self.res.get('subdir').provider().listdir()),
-                set(('__init__.py', 'a.json', 'subdir')))
+                set(('__init__.py', 'a.yml', 'subdir')))
         self.assertEqual(
-                set(self.res.get(['subdir', 'b.json']).provider().listdir()),
-                set(('b.json',)))
+                set(self.res.get(['subdir', 'b.yml']).provider().listdir()),
+                set(('b.yml',)))
 
     def test_filter(self):
         self.assertEqual(
                 set(self.res.listdir_filter(ext='.txt')),
                 set())
         self.assertEqual(
-                set(self.res.listdir_filter(ext='.json')),
-                set(('a.json',)))
+                set(self.res.listdir_filter(ext='.yml')),
+                set(('a.yml',)))
 
     def test_load(self):
         self.assertEqual(
-                json.loads(self.res.load('a.json', 'utf-8'))['path'],
-                'a.json')
+                yaml.load(self.res.load('a.yml', 'utf-8'))['path'],
+                'a.yml')
         self.assertEqual(
-                json.loads(self.res.get('subdir').load('b.json', 'utf-8'))['path'],
-                os.path.join('subdir', 'b.json'))
+                yaml.load(self.res.get('subdir').load('b.yml', 'utf-8'))['path'],
+                os.path.join('subdir', 'b.yml'))
 
-    def test_json(self):
+    def test_yaml(self):
         self.assertEqual(
-                self.res.json('a.json')['path'],
-                'a.json')
+                self.res.yaml('a.yml')['path'],
+                'a.yml')
         self.assertEqual(
-                self.res.get(['subdir', 'b.json']).json()['path'],
-                os.path.join('subdir', 'b.json'))
+                self.res.get(['subdir', 'b.yml']).yaml()['path'],
+                os.path.join('subdir', 'b.yml'))
 
 
 # test cases
@@ -151,17 +151,17 @@ class TestPackageResource(Common, unittest.TestCase):
         super(TestPackageResource, self).tearDown()
 
     def test_filename(self):
-        with self.res.filename('a.json') as filename:
+        with self.res.filename('a.yml') as filename:
             with open(filename, encoding='utf-8') as f:
                 self.assertEqual(
-                        json.loads(f.read())['path'],
-                        'a.json')
+                        yaml.load(f.read())['path'],
+                        'a.yml')
         self.assertTrue(os.path.exists(filename))
-        with self.res.get(['subdir', 'b.json']).filename() as filename:
+        with self.res.get(['subdir', 'b.yml']).filename() as filename:
             with open(filename, encoding='utf-8') as f:
                 self.assertEqual(
-                        json.loads(f.read())['path'],
-                        'subdir/b.json')
+                        yaml.load(f.read())['path'],
+                        'subdir/b.yml')
         self.assertTrue(os.path.exists(filename))
 
 class TestEggResource(Common, unittest.TestCase):
@@ -176,8 +176,8 @@ class TestEggResource(Common, unittest.TestCase):
                     packages=[self.mod],
                     script_args=['bdist_egg', '--quiet'],
                     package_data={self.mod:[
-                        'a.json',
-                        os.path.join('subdir', 'b.json')]})
+                        'a.yml',
+                        os.path.join('subdir', 'b.yml')]})
         os.chdir(cwd)
         self.eggs = os.listdir(os.path.join(self.base, 'dist'))
         for egg in self.eggs:
@@ -192,25 +192,25 @@ class TestEggResource(Common, unittest.TestCase):
         super(TestEggResource, self).tearDown()
 
     def test_filename(self):
-        with self.res.filename('a.json') as filename:
+        with self.res.filename('a.yml') as filename:
             with open(filename, encoding='utf-8') as f:
                 self.assertEqual(
-                        json.loads(f.read())['path'],
-                        'a.json')
+                        yaml.load(f.read())['path'],
+                        'a.yml')
         self.assertFalse(os.path.exists(filename))
-        with self.res.get(['subdir', 'b.json']).filename() as filename:
+        with self.res.get(['subdir', 'b.yml']).filename() as filename:
             with open(filename, encoding='utf-8') as f:
                 self.assertEqual(
-                        json.loads(f.read())['path'],
-                        'subdir/b.json')
+                        yaml.load(f.read())['path'],
+                        'subdir/b.yml')
         self.assertFalse(os.path.exists(filename))
         # The resource should be accessible again, even though the file was
         # deleted when exiting the context manager:
-        with self.res.get(['subdir', 'b.json']).filename() as filename:
+        with self.res.get(['subdir', 'b.yml']).filename() as filename:
             with open(filename, encoding='utf-8') as f:
                 self.assertEqual(
-                        json.loads(f.read())['path'],
-                        'subdir/b.json')
+                        yaml.load(f.read())['path'],
+                        'subdir/b.yml')
         self.assertFalse(os.path.exists(filename))
 
 class TestFileResource(Common, unittest.TestCase):
@@ -223,14 +223,14 @@ class TestFileResource(Common, unittest.TestCase):
         super(TestFileResource, self).tearDown()
 
     def test_filename(self):
-        with self.res.filename('a.json') as filename:
+        with self.res.filename('a.yml') as filename:
             self.assertEqual(
                     filename,
-                    os.path.join(self.path, 'a.json'))
-        with self.res.get(['subdir', 'b.json']).filename() as filename:
+                    os.path.join(self.path, 'a.yml'))
+        with self.res.get(['subdir', 'b.yml']).filename() as filename:
             self.assertEqual(
                     filename,
-                    os.path.join(self.path, 'subdir', 'b.json'))
+                    os.path.join(self.path, 'subdir', 'b.yml'))
 
 
 class TestCouchResource(unittest.TestCase):
