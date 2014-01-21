b73486797638e80ae9694ba16c917f149f4c33e2
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2014-01-21 11:47:31 +0100

    tests should fail if FAILED found in output (even if test return value is 0)

--- test/cpymadtest/CTestTestfile.cmake
+++ test/cpymadtest/CTestTestfile.cmake
@@ -1,6 +1,7 @@
 
-foreach(PYTEST clic aperture survey)
+foreach(PYTEST clic aperture survey madx resource model_locator)
    add_test(test_${PYTEST} ${PYTHON} test_${PYTEST}.py)
+   set_tests_properties(test_${PYTEST} PROPERTIES FAIL_REGULAR_EXPRESSION "FAILED")
 endforeach()
 
 # long test, can be excluded by running
@@ -9,7 +10,3 @@ foreach(PYTEST lhc-b4 lhc)
    add_test(test_${PYTEST} ${PYTHON} test_${PYTEST}.py)
    set_tests_properties(test_${PYTEST} PROPERTIES LABELS SLOW FAIL_REGULAR_EXPRESSION "FAILED" TIMEOUT 900)
 endforeach()
-
-add_test(test_madx ${PYTHON} test_madx.py)
-add_test(test_resource ${PYTHON} test_resource.py)
-add_test(test_model_locator ${PYTHON} test_model_locator.py)
