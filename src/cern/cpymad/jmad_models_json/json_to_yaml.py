f23d1bf1aab22bea1dee30d48c849fcce560e130
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2014-01-21 09:11:37 +0100

    simpler way of getting rid of python/unicode in yaml files see issue #34

--- src/cern/cpymad/jmad_models_json/json_to_yaml.py
+++ src/cern/cpymad/jmad_models_json/json_to_yaml.py
@@ -2,51 +2,11 @@
 import json,yaml,os
 
 
-def _decode_list(data):
-    '''
-    Borrowed from
-    http://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-of-unicode-ones-from-json-in-python
-
-    Need this to get normal string objects in the dictionary instead of unicode ones
-    (which makes the yaml file much harder to read)
-    '''
-    rv = []
-    for item in data:
-        if isinstance(item, unicode):
-            item = item.encode('utf-8')
-        elif isinstance(item, list):
-            item = _decode_list(item)
-        elif isinstance(item, dict):
-            item = _decode_dict(item)
-        rv.append(item)
-    return rv
-
-def _decode_dict(data):
-    '''
-    Borrowed from
-    http://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-of-unicode-ones-from-json-in-python
-
-    Need this to get normal string objects in the dictionary instead of unicode ones
-    (which makes the yaml file much harder to read)
-    '''
-    rv = {}
-    for key, value in data.iteritems():
-        if isinstance(key, unicode):
-            key = key.encode('utf-8')
-        if isinstance(value, unicode):
-            value = value.encode('utf-8')
-        elif isinstance(value, list):
-            value = _decode_list(value)
-        elif isinstance(value, dict):
-            value = _decode_dict(value)
-        rv[key] = value
-    return rv
-
 
 if __name__ == "__main__":
     folder='../_models/'
     for f in os.listdir(folder):
         if f[-12:]=='.cpymad.json':
             print("Converting "+f[:-12])
-            d=json.load(file(folder+f),object_hook=_decode_dict)
-            yaml.dump(d,stream=file(folder+f[:-4]+'yml','w'))
+            d=json.load(file(folder+f))
+            yaml.safe_dump(d,stream=file(folder+f[:-4]+'yml','w'))
