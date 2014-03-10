3dd335c7f56fa0b162adeb9ffc3db30e975ea326
Thomas Gläßle <t_glaessle@gmx.de>
2014-03-10 08:01:19 +0100

    Improve fd reallocation safety
    The algorithm should now always be able to swap the files associated to
    the original stdio file descriptors correctly.

--- src/cern/cpymad/_libmadx_rpc.py
+++ src/cern/cpymad/_libmadx_rpc.py
@@ -34,36 +34,56 @@ except ImportError:
     # http://docs.python.org/3.3/whatsnew/3.0.html?highlight=cpickle
     import pickle
 
-def reopen_stdio():
+def remap_stdio():
     """
-    Reopen the standard input and output streams.
+    Remap STDIO streams to new file descriptors and create new STDIO streams.
+
+    Create new file descriptors for the original STDIO streams. Then
+    replace the python STDIO file objects with newly opened streams:
 
     :obj:`sys.stdin` is mapped to a NULL stream.
     :obj:`sys.stdout` is initialized with the current console.
 
-    CAUTION: the streams are opened in binary mode! This deviates from the
-    default on python3!
+    :returns: the remapped (STDIN, STDOUT) file descriptors
 
     """
-    STDIN = 0       # POSIX file descriptor == sys.stdin.fileno()
-    STDOUT = 1
+    # This function can only make sure that the original file descriptors
+    # of sys.stdin, sys.stdout, sys.stderr are remapped correctly. It can
+    # make no guarantees about the standard POSIX file descriptors (0, 1).
+    # Usually though, these should be the same.
+    STDIN = sys.stdin.fileno()
+    STDOUT = sys.stdout.fileno()
     # virtual file name for console (terminal) IO:
     console = 'con:' if sys.platform == 'win32' else '/dev/tty'
+    stdin_fd = os.open(os.devnull, os.O_RDONLY)
+    try:
+        stdout_fd = os.open(console, os.O_WRONLY)
+    except (IOError, OSError):
+        stdout_fd = os.open(os.devnull, os.O_WRONLY)
+    # The original stdio streams can only be closed *after* opening new
+    # stdio streams to avoid the risk that the file descriptors will be
+    # reused immediately. But before closing, their file descriptors need
+    # to be duplicated:
+    recv_fd = os.dup(sys.stdin.fileno())
+    send_fd = os.dup(sys.stdout.fileno())
+    sys.stdin.close()
+    sys.stdout.close()
+    # By duplicating the file descriptors to the STDIN/STDOUT file
+    # descriptors non-python libraries can make use of these streams as
+    # well. The initial fds are not needed anymore.
+    os.dup2(stdin_fd, STDIN)
+    os.dup2(stdout_fd, STDOUT)
+    os.close(stdin_fd)
+    os.close(stdout_fd)
     # Create new python file objects for STDIN/STDOUT and remap the
     # corresponding file descriptors: Reopen python standard streams. This
     # enables all python modules to use these streams. Note: the stdout
     # buffer length is set to '1', making it line buffered, which behaves
     # like the default in most circumstances.
-    sys.stdin = open(os.devnull, 'rt')
-    try:
-        sys.stdout = open(console, 'wt', 1)
-    except (IOError, OSError):
-        sys.stdout = open(os.devnull, 'wt', 1)
-    # By duplicating the file descriptors to the STDIN/STDOUT file
-    # descriptors non-python libraries can make use of these streams as
-    # well:
-    os.dup2(sys.stdin.fileno(), STDIN)
-    os.dup2(sys.stdout.fileno(), STDOUT)
+    sys.stdin = os.fdopen(STDIN, 'rt')
+    sys.stdout = os.fdopen(STDOUT, 'wt', 1)
+    # Return the remapped file descriptors of the original STDIO streams
+    return recv_fd, send_fd
 
 
 class Connection(object):
@@ -217,8 +237,7 @@ class Service(object):
     @classmethod
     def stdio_main(cls):
         """Do the full job of preparing and running an RPC service."""
-        conn = Connection.from_stream(sys.stdin, sys.stdout)
-        reopen_stdio()
+        conn = Connection.from_fd(*remap_stdio())
         cls(conn).run()
 
     def run(self):
