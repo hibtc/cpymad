a37405d0177e62089b75b27d85dc0deda6ad978e
Thomas Gläßle <t_glaessle@gmx.de>
2014-07-16 16:47:39 +0200

    Fix typo

--- src/cern/cpymad/libmadx.pyx
+++ src/cern/cpymad/libmadx.pyx
@@ -64,8 +64,8 @@ def _get_rightmost_word(sentence):
 
 
 # MAD-X version introspection
-madx_release = _get_right_word(_str(clib.version_name))
-madx_date = _get_right_word(_str(clib.version_date_mod))
+madx_release = _get_rightmost_word(_str(clib.version_name))
+madx_date = _get_rightmost_word(_str(clib.version_date_mod))
 
 
 def started():
