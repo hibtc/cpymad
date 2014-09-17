e7641a38378b2627285ea6fca8282ba38ca2143d
Thomas Gläßle <t_glaessle@gmx.de>
2014-09-17 19:33:45 +0200

    Fix problem with pip and ez_setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,14 @@
 # limitations under the License.
 #-------------------------------------------------------------------------------
 
-# Make sure setuptools is available:
-from ez_setup import use_setuptools
-use_setuptools()
+# Make sure setuptools is available. NOTE: the try/except hack is required to
+# make installation work with pip: If an older version of setuptools is
+# already imported, `use_setuptools()` will just exit the current process.
+try:
+    import pkg_resources
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
 
 from setuptools import setup, Extension
 from distutils.util import get_platform
