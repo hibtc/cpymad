73514adabb7ae1778cf827365a76f9c42e5e15ce
Thomas Gläßle <t_glaessle@gmx.de>
2014-11-23 13:21:10 +0100

    Remove boilerplate COPYRIGHT notices
    These do not resolve the true modification history very well. For more
    details see the version history (git).
    
    All contributions by Thomas Gläßle are CC0 and some of these files are
    complete rewrites.

--- cern/resource/file.py
+++ cern/resource/file.py
@@ -1,14 +1,4 @@
 # encoding: utf-8
-#----------------------------------------
-# file.py by Thomas Gläßle
-# 
-# To the extent possible under law, the person who associated CC0 with
-# file.py has waived all copyright and related or neighboring rights
-# to file.py.
-# 
-# You should have received a copy of the CC0 legalcode along with this
-# work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-#----------------------------------------
 """
 Resource provider for plain filesystem resources.
 """
