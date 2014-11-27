c1292948c039b377c9cb5b8dfd575f0604eeca58
Thomas Gläßle <t_glaessle@gmx.de>
2014-11-27 03:09:04 +0100

    Move TestMRO into separate test module

--- test/test_locator.py
+++ test/test_locator.py
@@ -4,7 +4,7 @@ Tests for the classes defined in cern.cpymad.model_locator.
 """
 __all__ = ['TestMergedModelLocator',
            'TestChainedModelLocator',
-           'TestMROFunc']
+           ]
 
 # tested classes
 from cern.cpymad.model_locator import ModelData, MergedModelLocator, ChainModelLocator, C3_mro
@@ -189,87 +189,6 @@ class TestMergedModelLocator(unittest.TestCase):
         self.assertEqual(c.get_by_dict(cf[2]).yaml()['path'],
                          'dbdir/c/c.txt')
 
-
-
-class TestMROFunc(unittest.TestCase):
-    """
-    Test the C3_mro() function.
-
-    Note: the tests will only succeed for versions of python where C3 is
-    used, i.e. python>=2.3.
-
-    """
-    def mro(self, *bases):
-        return tuple(C3_mro(lambda cls: cls.__bases__, *bases))
-
-    def test_SeriousOrderDisagreement(self):
-        O = object
-        class X(O): pass
-        class Y(O): pass
-        class A(X, Y): pass
-        class B(Y, X): pass
-        bases = (A, B)
-        self.assertRaises(TypeError, self.mro, A, B)
-
-    def test_TrivialSingleInheritance(self):
-        O = object
-        class A(O): pass
-        class B(A): pass
-        class C(B): pass
-        class D(C): pass
-        self.assertEqual(self.mro(D), D.__mro__)
-
-    def test_Example1(self):
-        O = object
-        class F(O): pass
-        class E(O): pass
-        class D(O): pass
-        class C(D, F): pass
-        class B(D, E): pass
-        class A(B, C): pass
-        self.assertEqual(self.mro(A), A.__mro__)
-
-    def test_Example2(self):
-        O = object
-        class F(O): pass
-        class E(O): pass
-        class D(O): pass
-        class C(D, F): pass
-        class B(E, D): pass
-        class A(B, C): pass
-        self.assertEqual(self.mro(A), A.__mro__)
-
-    def test_Example3(self):
-        O = object
-        class A(O): pass
-        class B(O): pass
-        class C(O): pass
-        class D(O): pass
-        class E(O): pass
-        class K1(A, B, C): pass
-        class K2(D, B, E): pass
-        class K3(D, A): pass
-        class Z(K1, K2, K3): pass
-        self.assertEqual(self.mro(A), A.__mro__)
-        self.assertEqual(self.mro(B), B.__mro__)
-        self.assertEqual(self.mro(C), C.__mro__)
-        self.assertEqual(self.mro(D), D.__mro__)
-        self.assertEqual(self.mro(E), E.__mro__)
-        self.assertEqual(self.mro(K1), K1.__mro__)
-        self.assertEqual(self.mro(K2), K2.__mro__)
-        self.assertEqual(self.mro(K3), K3.__mro__)
-        self.assertEqual(self.mro(Z), Z.__mro__)
-
-
-
-class TestChainedModelLocator(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-
-    def test_(self):
-        pass
 
 if __name__ == '__main__':
     unittest.main()
