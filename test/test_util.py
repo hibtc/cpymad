17a0bab9d5032ce65a2dbf5cabef3708d7cb1b74
Thomas Gläßle <t_glaessle@gmx.de>
2016-09-15 09:53:11 +0200

    Add a few tests for command formatting

--- test/test_util.py
+++ test/test_util.py
@@ -6,6 +6,7 @@ import _compat
 
 # tested objects
 from cpymad import util
+from cpymad.types import Range, Constraint, Expression
 
 
 class TestUtil(unittest.TestCase, _compat.TestCase):
@@ -35,6 +36,35 @@ class TestUtil(unittest.TestCase, _compat.TestCase):
         self.assertEqual(util.normalize_range_name('dr[1]'), 'dr[1]')
         self.assertEqual(util.normalize_range_name('lebt$end'), '#e')
         self.assertEqual(util.normalize_range_name('dr'), 'dr')
+
+    def _check_command(self, compare, *args, **kwargs):
+        self.assertEqual(compare, util.mad_command(*args, **kwargs))
+
+    def test_mad_command(self):
+        self.assertEqual(
+            util.mad_command(
+                'twiss', sequence='lhc'),
+                "twiss, sequence='lhc';")
+        self.assertEqual(
+            util.mad_command(
+                'option', echo=True),
+                'option, echo;')
+        self.assertEqual(
+            util.mad_command(
+                'constraint', betx=Constraint(max=3.13)),
+                'constraint, betx<3.13;')
+        self.assertEqual(
+            util.mad_command(
+                'quadrupole', k1=Expression('pi/2', 3.14/2)),
+                'quadrupole, k1:=pi/2;')
+        self.assertEqual(
+            util.mad_command(
+                'multipole', knl=[0.0, 1.0, 2.0]),
+                'multipole, knl={0.0,1.0,2.0};')
+        self.assertEqual(
+            util.mad_command(
+                'twiss', range=Range('#s', '#e')),
+                'twiss, range=#s/#e;')
 
     # TODO: test other functions
 
