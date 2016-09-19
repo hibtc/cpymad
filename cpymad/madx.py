cb94016704daf350fa6e52e1f02b97a47024c4bd
Thomas Gläßle <t_glaessle@gmx.de>
2016-09-19 10:47:40 +0200

    Move ChangeDirectory to minrpc

--- cpymad/madx.py
+++ cpymad/madx.py
@@ -14,6 +14,8 @@ import collections
 
 import numpy as np
 
+from minrpc.util import ChangeDirectory
+
 from . import _rpc
 from . import util
 
@@ -35,37 +37,6 @@ class Version(object):
     def __repr__(self):
         """Show nice version string to user."""
         return "MAD-X {} ({})".format(self.release, self.date)
-
-
-class ChangeDirectory(object):
-
-    """
-    Context manager for temporarily changing current working directory in the
-    context of the given ``os`` module.
-
-    :param str path: new path name
-    :param _os: module with ``getcwd`` and ``chdir`` functions
-    """
-
-    def __init__(self, path, _os):
-        self._os = _os
-        # Contrary to common implementations of a similar context manager,
-        # we change the path immediately in the constructor. That enables
-        # this utility to be used without any 'with' statement:
-        if path:
-            self._restore = _os.getcwd()
-            _os.chdir(path)
-        else:
-            self._restore = None
-
-    def __enter__(self):
-        """Enter 'with' context."""
-        pass
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        """Exit 'with' context and restore old path."""
-        if self._restore:
-            self._os.chdir(self._restore)
 
 
 class MadxCommands(object):
