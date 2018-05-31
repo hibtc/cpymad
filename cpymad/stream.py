d81841e2c3fbf632aee04078b354f09277c8d3c8
Thomas Gläßle <t_glaessle@gmx.de>
2018-05-31 20:29:57 +0200

    Revert "Read MAD-X synchronously for now"
    This reverts commit 4f319c8069622a2ca658d1f0ccec475a2164efb2.

--- cpymad/stream.py
+++ cpymad/stream.py
@@ -1,3 +1,6 @@
+from multiprocessing.dummy import Pool as ThreadPool
+
+
 # The following code makes sure to read all available stdout lines before
 # sending more input to MAD-X (ensure the real chronological order!), see:
 #       linux:   https://stackoverflow.com/q/375427/650222
@@ -31,41 +34,38 @@ except ImportError:         # Windows
             raise OSError(WinError())
 
 
-class StreamReader:
+class AsyncReader:
 
     """Read stream asynchronously in a worker thread. Note that the worker
     thread will only be active while have entered the `with` context."""
 
-    # NOTE: If MAD-X writes too much output to its STDOUT pipe without any
-    # consumer running in parallel, the write will block -- leading to a
-    # deadlock. Therefore we *should* ensure to read the pipe while waiting
-    # for MAD-X commands to finish. However, any threaded solution I came up
-    # with that is able to keep the chronological order had a *huge*
-    # performance deficit (every command takes ~100ms) -- which is why we
-    # currently stay with a single-threaded solution that keeps the
-    # chronological order but could potentially deadlock.
-
     def __init__(self, stream, callback):
         super().__init__()
         set_nonblocking(stream)
+        self.pool = ThreadPool(1)
         self.stream = stream
         self.callback = callback
 
     def __enter__(self):
-        pass
+        self.stop = False
+        self.result = self.pool.apply_async(self._read_thread)
 
     def __exit__(self, *exc_info):
+        self.stop = True
+        self.callback("\n".join(self.result.get()))
+
+    def _read_thread(self):
         lines = []
         while True:
             try:
                 line = self.stream.readline()
             except IOError:
-                break
+                if self.stop:
+                    return lines
+                continue
             if not line:
-                break
+                return lines
             lines.append(line.decode('utf-8', 'replace')[:-1])
-        if lines:
-            self.callback("\n".join(lines))
 
     def flush(self):
         """Read all data from the remote."""
