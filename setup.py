02337bab43ed01ac7415fd7d518b37b0ea70c3e2
Thomas Gläßle <t_glaessle@gmx.de>
2018-07-25 14:25:57 +0200

    Change setup.py to autobuild MAD-X

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ For more information, see
 """
 
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 from distutils.util import get_platform, convert_path
 from distutils import sysconfig
+from distutils.errors import PreprocessError, CompileError
 
-import itertools
+from textwrap import dedent
 import sys
-from os import path
+import os
 
 
 try:
@@ -35,6 +37,108 @@ def fix_distutils_sysconfig_mingw():
     """
     if sysconfig.get_config_var('CC') is None:
         sysconfig._config_vars['CC'] = 'gcc'
+
+
+class build_ext(_build_ext):
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.madxdir = madxdir
+        self.static = static
+
+    def build_extension(self, ext):
+        ext.__dict__.update(get_extension_args(self.madxdir, self.static))
+        try:
+            # Return if the extension has already been built or MAD-X is
+            # available. This prevents us from unnecessary work for example
+            # when performing the `install` stage without passing `--madxdir`
+            # after already having done `build_ext` successfully:
+            return super().build_extension(ext)
+        # NOTE that we don't catch LinkerError since that indicates that MAD-X
+        # is available but somehow missconfigured and we should never attempt
+        # to download/rebuild in that case:
+        except (PreprocessError, CompileError) as e:
+            # Stop if the user had specified `--madxdir` or if the error is
+            # not due to the inavailability of MAD-X. In these cases we should
+            # not start trying to build MAD-X:
+            if self.has_madx():
+                raise
+        print(dedent("""
+
+        Could not find MAD-X development files.
+
+        We will now try to download and build MAD-X for you. This may
+        take several minutes (depending on your internet connection
+        and overall system performance), and requires recent versions
+        of the following programs:
+
+            - cmake
+            - gcc
+            - gfortran
+
+        In case of problems, please build MAD-X manually and then pass
+        the installation directory using the `--madxdir` argument to
+        `setup.py`. For further information please check the
+        installation instructions at:
+
+            http://hibtc.github.io/cpymad/installation/index.html
+
+        """), file=sys.stderr)
+        self.build_madx(int(os.environ.get('MADX_STATIC', '1')))
+        ext.__dict__.update(get_extension_args(self.madxdir, self.static))
+        return super().build_extension(ext)
+
+    def has_madx(self):
+        return self.madxdir or self.check_dependency(dedent("""
+        #include "madX/madx.h"
+        int main(int argc, char* argv[])
+        {
+            madx_start();
+            return 0;
+        }
+        """))
+
+    def build_madx(self, static):
+        sys.path.append('utils')
+        from build_madx import install_madx
+        sys.path.remove('utils')
+        self.static = static
+        self.madxdir = install_madx(prefix=self.build_temp, static=static)
+
+    def check_dependency(self, c_code):
+        """Check if an external library can be found by trying to compile a
+        small program."""
+        # This method is needed to prevent us from downloading and building
+        # MAD-X on every error in the C code itself during development even
+        # if MAD-X is available.
+        print(dedent("""
+
+        No `--madxdir` specified. We will now try to check if the MAD-X
+        development files are already installed in a system directory.
+
+        If you have installed MAD-X in a non-standard or user directory,
+        please pass the `--madxdir=PATH` argument to `setup.py`.
+
+        """), file=sys.stderr)
+        import tempfile
+        import shutil
+        tmp_dir = tempfile.mkdtemp(prefix='tmp_cpymad_madx_')
+        tmp_bin = os.path.join(tmp_dir, 'test_madx')
+        tmp_src = tmp_bin + '.c'
+        with open(tmp_src, 'w') as f:
+            f.write(c_code)
+        ext_args = get_extension_args(self.madxdir, self.static)
+        try:
+            self.compiler.compile(
+                [tmp_src],
+                output_dir=tmp_dir,
+                include_dirs=ext_args['include_dirs'],
+                extra_postargs=ext_args['extra_compile_args'])
+            return True
+        except (PreprocessError, CompileError) as e:
+            return False
+        finally:
+            shutil.rmtree(tmp_dir)
 
 
 def read_file(path):
@@ -97,16 +201,14 @@ def remove_arg(args, opt):
             return arg.split('=', 1)[1]
 
 
-def get_extension_args(argv):
+def get_extension_args(madxdir, static):
     """Get arguments for C-extension (include pathes, libraries, etc)."""
-    # Let's just use the default system headers:
     include_dirs = []
     library_dirs = []
     # Parse command line option: --madxdir=/path/to/madxinstallation. We could
     # use build_ext.user_options instead, but then the --madxdir argument can
     # be passed only to the 'build_ext' command, not to 'build' or 'install',
     # which is a minor nuisance.
-    madxdir = remove_arg(argv, '--madxdir')
     if madxdir:
         prefix = os.path.expanduser(madxdir)
         include_dirs += [os.path.join(prefix, 'include')]
@@ -118,9 +220,7 @@ def get_extension_args(argv):
     # Mac:      darwin*
     platform = get_platform()
 
-    static = '--static' in argv
     if static:
-        argv.remove('--static')
         libraries = ['madx', 'ptc', 'gc-lib',
                      'stdc++', 'gfortran', 'quadmath']
         # NOTE: If MAD-X was built with BLAS/LAPACK, you must manually provide
@@ -142,7 +242,6 @@ def get_extension_args(argv):
 
 def get_setup_args(argv):
     """Accumulate metadata for setup."""
-    extension_args = get_extension_args(argv)
     long_description = get_long_description()
     metadata = exec_file('cpymad/__init__.py')
     return dict(
@@ -160,7 +259,7 @@ def get_setup_args(argv):
         ext_modules = cythonize([
             Extension('cpymad.libmadx',
                       sources=["cpymad/libmadx.pyx"],
-                      **extension_args),
+                      libraries=['madx']),
         ]),
         packages = [
             "cpymad",
@@ -172,7 +271,13 @@ def get_setup_args(argv):
             'PyYAML',
             'minrpc>=0.0.7',
         ],
+        cmdclass={'build_ext': build_ext},
     )
+
+
+madxdir = remove_arg(sys.argv, '--madxdir')
+static = '--static' in sys.argv
+if static: sys.argv.remove('--static')
 
 
 def main():
