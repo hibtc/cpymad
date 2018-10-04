c946ba834d9409551282ecd843690f967607a460
Thomas Gläßle <t_glaessle@gmx.de>
2018-10-04 18:59:17 +0200

    Pass auth token to github api when querying refs
    To prevent github from blocking our request as spam.

--- utils/cleanup_artifacts.py
+++ utils/cleanup_artifacts.py
@@ -7,17 +7,21 @@ import os
 import re
 
 repo, build, num_keep = sys.argv[1:]
-slug = '/'.join(repo.split('/')[-2:])
+
+token, slug = re.match(
+    r'https://([0-9a-f]{40})@github.com/(.*)', repo).groups()
 min_build = int(build) - int(num_keep) + 1
 
 with open(os.devnull) as null:
     data = check_output([
-        'curl', 'https://api.github.com/repos/{}/git/refs/heads/'.format(slug),
+        'curl', '-H',
+        'Authorization: token {}'.format(token),
+        'https://api.github.com/repos/{}/git/refs/heads/'.format(slug),
     ], stderr=null)
 
 branches = [r['ref'].split('/', 2)[2] for r in json.loads(data)]
 
-travis = re.compile('_(\d+)-[0-9a-f]{40}')
+travis = re.compile(r'_(\d+)-[0-9a-f]{40}')
 
 cleanup = [
     ':' + branch
@@ -25,5 +29,6 @@ cleanup = [
     for m in [travis.match(branch)]
     if m and int(m.group(1)) < min_build
 ]
+print("Deleting:\n", "\n ".join(cleanup) or "(nothing)")
 
 run(['git', 'push', repo] + cleanup)
