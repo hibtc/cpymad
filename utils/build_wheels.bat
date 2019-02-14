7615bdcaca944c24992d80e4edea399f55625f7c
Thomas Gläßle <t_glaessle@gmx.de>
2019-02-14 19:31:20 +0100

    Must "call" conda within .bat files!
    Otherwise, since conda is a .bat script itself, the invoking .bat script
    will exit after conda.
    
    See: https://github.com/conda/conda/issues/6553

--- utils/build_wheels.bat
+++ utils/build_wheels.bat
@@ -5,21 +5,21 @@
 ::          python build_madx.py --static
 
 :: Create python environments:
-conda create -qy -n py27 python=2.7 wheel
-conda create -qy -n py33 python=3.3 wheel
-conda create -qy -n py34 python=3.4 wheel
-conda create -qy -n py35 python=3.5 wheel
-conda create -qy -n py36 python=3.6 wheel
-conda create -qy -n py37 python=3.7 wheel
+call conda create -qy -n py27 python=2.7 wheel
+call conda create -qy -n py33 python=3.3 wheel
+call conda create -qy -n py34 python=3.4 wheel
+call conda create -qy -n py35 python=3.5 wheel
+call conda create -qy -n py36 python=3.6 wheel
+call conda create -qy -n py37 python=3.7 wheel
 
 :: Install mingwpy where available:
-conda install -qy -n py27 -c conda-forge mingwpy
-conda install -qy -n py33 -c conda-forge mingwpy
-conda install -qy -n py34 -c conda-forge mingwpy
+call conda install -qy -n py27 -c conda-forge mingwpy
+call conda install -qy -n py33 -c conda-forge mingwpy
+call conda install -qy -n py34 -c conda-forge mingwpy
 
 :: Prepare cython source:
-activate py34
-conda install -qy cython
+call activate py34
+call conda install -qy cython
 cython src\cpymad\libmadx.pyx -I %MADXDIR%\include
 
 :: Locate gcc from mingwpy in py34 (used later for build_cpymad2):
@@ -51,7 +51,7 @@ exit /b %ERRORLEVEL%
 setlocal
     set "py_env=%1"
 
-    activate %py_env%
+    call activate %py_env%
     call python setup.py build_ext -c mingw32 --static
     call python setup.py bdist_wheel
 endlocal
@@ -66,7 +66,7 @@ setlocal
     set "dir_tag=%3"
     set "file_tag=%4"
 
-    activate %py_env%
+    call activate %py_env%
     set tempdir=build\temp.%dir_tag%\Release\src\cpymad
     set builddir=build\lib.%dir_tag%\cpymad
     mkdir %tempdir%
