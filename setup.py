f1f1250aebc91a886f591a3cd2b5c7097513ff85
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-05 12:15:38 +0100

    Simply put a link to the changelog

--- setup.py
+++ setup.py
@@ -83,37 +83,20 @@ def exec_file(path):
     return namespace
 
 
-def first_sections(document, heading_type, num_sections):
-    cur_section = 0
-    prev_line = None
-    for line in document.splitlines():
-        if line.startswith(heading_type*3) and set(line) == {heading_type}:
-            cur_section += 1
-            if cur_section == num_sections:
-                break
-        if prev_line is not None:
-            yield prev_line
-        prev_line = line
-
-
 def get_long_description():
     """Compose a long description for PyPI."""
-    long_description = None
     try:
-        long_description = read_file('README.rst').decode('utf-8')
-        changelog = read_file('CHANGES.rst').decode('utf-8')
-        changelog = "\n".join(first_sections(changelog, '=', 4)) + """
-Older versions
-==============
+        return read_file('README.rst').decode('utf-8') + """
+
+CHANGELOG
+=========
 
 The full changelog is available online in CHANGES.rst_.
 
 .. _CHANGES.rst: https://github.com/hibtc/cpymad/blob/master/CHANGES.rst
 """
-        long_description += '\n' + changelog
     except (IOError, UnicodeDecodeError):
-        pass
-    return long_description
+        return None
 
 
 def get_extension_args(madxdir, shared, static, **libs):
