5ea5776166f4730834f60cc5eceda837808b0545
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-07 02:30:46 +0100

    Substitute 7z/pywget using powershell builtins
    Probably a bad idea.
    
    Note that we have to use the zip archive instead of tar.gz because
    powershell's Expand-Archive can't extract .tar.gz natively.

--- utils/build_madx.ps1
+++ utils/build_madx.ps1
@@ -2,21 +2,24 @@
 $MADXDIR = if ($env:MADXDIR) { $env:MADXDIR } else { "madx-bin" }
 $MADXDIR = [System.IO.Path]::Combine($(pwd), $MADXDIR)
 
-# Acquire MAD-X:
 $MADX_VER = "5.04.02"
 $MADX_ZIP = "$MADX_VER.zip"
 $MADX_URL = "https://github.com/MethodicalAcceleratorDesign/MAD-X/archive/"
 $MADX_DIR = "MAD-X-$MADX_VER"
-& call python -m wget "$MADX_URL/$MADX_ZIP" -o $MADX_ZIP
-& call 7za x $MADX_ZIP
 
-:: Patch garbage collector to newer version, see #41:
 $GC_VER = "8.0.2"
-$GC_ZIP = "gc-$GC_VER.tar.gz"
-$GC_URL = "https://github.com/ivmai/bdwgc/releases/download/"
-& python -m wget "$GC_URL/v$GC_VER/$GC_ZIP" -o $GC_ZIP
-& 7za x $GC_ZIP
-& 7za x -o$MADX_DIR\libs\gc gc-$GC_VER.tar
+$GC_ZIP = "v$GC_VER.zip"
+$GC_URL = "https://github.com/ivmai/bdwgc/archive/"
+$GC_DIR = "$MADX_DIR\libs\gc\gc-$GC_VER"
+
+$web = New-Object System.Net.WebClient
+$web.DownloadFile("$MADX_URL/$MADX_ZIP", $MADX_ZIP)
+$web.DownloadFile("$GC_URL/$GC_ZIP", $GC_ZIP)
+Expand-Archive -LiteralPath $MADX_ZIP -DestinationPath .
+Expand-Archive -LiteralPath $GC_ZIP -DestinationPath .
+mv bdwgc-$GC_VER $GC_DIR
+
+# Patch garbage collector to newer version, see #41:
 & patch -d $MADX_DIR -p1 -i "$PSScriptRoot\patches\gc-8.0.2.diff"
 
 # Build MAD-X as library:
