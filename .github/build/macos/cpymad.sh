0f52fdeec3173101288874eee58960992090b858
Thomas Gläßle <t_glaessle@gmx.de>
2020-10-07 20:48:25 +0200

    Set MACOSX_DEPLOYMENT_TARGET

--- .github/build/macos/cpymad.sh
+++ .github/build/macos/cpymad.sh
@@ -3,4 +3,4 @@ export CC=gcc-9
 export MADXDIR=../MAD-X/dist
 export BLAS=1 LAPACK=1
 pip install cython wheel
-python setup.py sdist bdist_wheel -p macosx_10_9_x86_64
+python setup.py sdist bdist_wheel
