716b1297e037a0a271dbfd43a82f92161dc25adf
Thomas Gläßle <t_glaessle@gmx.de>
2021-08-13 15:27:21 +0200

    Put transfer map tests in their own file

--- test/test_madx.py
+++ test/test_madx.py
@@ -2,7 +2,6 @@
 Tests for the :class:`cpymad.madx.Madx` API.
 """
 
-# standard library
 import os
 import sys
 from textwrap import dedent
@@ -11,7 +10,6 @@ import numpy as np
 from numpy.testing import assert_allclose
 from pytest import approx, fixture, mark, raises
 
-# tested class
 import cpymad
 from cpymad.madx import Madx, metadata
 
@@ -844,113 +842,6 @@ def test_errors(mad):
     assert_allclose(fd.dks, expected_dks)
     assert_allclose(al.dx, 1e-3)
     assert_allclose(al.dy, -4e-3)
-
-
-def _test_transfer_map(mad, seq, range_, doc, rtol=1e-7, atol=1e-15):
-    mad.input(doc)
-    mad.use(seq)
-    par = ['x', 'px', 'y', 'py', 't', 'pt']
-    val = [+0.0010, -0.0015, -0.0020, +0.0025, +0.0000, +0.0000]
-    twiss = {'betx': 0.0012, 'alfx': 0.0018,
-             'bety': 0.0023, 'alfy': 0.0027}
-    twiss.update(zip(par, val))
-    elems = range_.split('/')
-    smap = mad.sectormap(elems, sequence=seq, **twiss)[-1]
-    tw = mad.twiss(sequence=seq, range=range_, **twiss)
-
-    # transport of coordinate vector:
-    x_init = np.array(val)
-    x_final_tw = np.array([tw[p][-1] for p in par])
-    x_final_sm = np.dot(smap, np.hstack((x_init, 1)))
-    assert_allclose(x_final_tw[:4], x_final_sm[:4],
-                    rtol=rtol, atol=atol)
-
-    # transport of beam matrix:
-    tm = smap[0:6, 0:6]
-    tab_len = len(tw['sig11'])
-    sig_init = tw.sigmat(0)
-    sig_final_tw = tw.sigmat(tab_len-1)
-    sig_final_sm = np.dot(tm, np.dot(sig_init, tm.T))
-    assert_allclose(sig_final_tw[0:4, 0:4], sig_final_sm[0:4, 0:4],
-                    rtol=rtol, atol=atol)
-
-
-def test_drift(mad):
-    _test_transfer_map(mad, 's', '#s/#e', """
-        s: sequence, l=1, refer=entry;
-        d: drift, l=1, at=0;
-        endsequence;
-        beam;
-    """)
-
-
-def test_kicker(mad):
-    # hkicker
-    _test_transfer_map(mad, 's', '#s/#e', """
-        s: sequence, l=3, refer=entry;
-        k: hkicker, kick=0.01, at=1;
-        endsequence;
-        beam;
-    """)
-    # vkicker
-    _test_transfer_map(mad, 's', '#s/#e', """
-        s: sequence, l=3, refer=entry;
-        k: vkicker, kick=0.01, at=1;
-        endsequence;
-        beam;
-    """)
-    # three identical drifts in between
-    _test_transfer_map(mad, 's', '#s/#e', """
-        s: sequence, l=3, refer=entry;
-        k: hkicker, kick=0.01, at=1;
-        m: monitor, at=2;
-        endsequence;
-        beam;
-    """)
-
-
-def test_quadrupole(mad):
-    _test_transfer_map(mad, 's', '#s/#e', """
-        qq: quadrupole, k1=0.01, l=1;
-        s: sequence, l=4, refer=entry;
-        qq, at=1;
-        qq, at=2;
-        endsequence;
-        beam;
-    """)
-
-
-def test_sbend(mad):
-    _test_transfer_map(mad, 's', '#s/#e', """
-        s: sequence, l=3, refer=entry;
-        b: sbend, angle=0.1, l=1, at=1;
-        endsequence;
-        beam;
-    """, rtol=3e-4)
-
-
-def test_solenoid(mad):
-    _test_transfer_map(mad, 's', '#s/#e', """
-        s: sequence, l=3, refer=entry;
-        n: solenoid, ks=1, l=1, at=1;
-        endsequence;
-        beam;
-    """)
-
-
-def test_subrange(mad):
-    _test_transfer_map(mad, 's', 'm1/m2', """
-        qq: quadrupole, k1=0.01, l=1;
-        s: sequence, l=6, refer=entry;
-        q0: quadrupole, k1=-0.01, l=0.5, at=0.25;
-        m1: marker, at=1;
-        qq, at=1;
-        qq, at=3;
-        m2: marker, at=5;
-        qq, at=5;
-        endsequence;
-        beam;
-    """)
 
 
 def _get_elems(mad, seq_name):
