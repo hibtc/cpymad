ee7fa901887080ee5efec093044190d9ca5379c6
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-16 19:39:25 +0100

    Simplify manylinux build scripts
    Now that we don't have to worry so much about permissions anymore.

--- .github/build/manylinux1_x86_64/cpymad.sh
+++ .github/build/manylinux1_x86_64/cpymad.sh
@@ -11,27 +11,17 @@ export X11=0 BLAS=0 LAPACK=0
 export CFLAGS="-fno-lto"
 export LDFLAGS="-fno-lto"
 
-# Copy the cpymad source files to a build folder in order to avoid permission
-# issues with the host filesystem (on both sides):
-mkdir -p build
-$PY/python setup.py egg_info
-tar -c $(cat src/cpymad.egg-info/SOURCES.txt) |
-    tar -x -C build --no-same-owner
-
 # We create the wheels from the source distribution to verify that the
 # source distribution can be used as installation medium. We will later
 # upload this exact source distribution to PyPI:
-pushd build
 python setup.py sdist
 
 for PYBIN in /opt/python/cp3*/bin; do
-    "${PYBIN}/pip" wheel dist/*.tar.gz --no-deps -w dist/
+    "${PYBIN}/pip" wheel dist/*.tar.gz --no-deps -w rawdist/
 done
-popd
 
 # Bundle external shared libraries into the wheels
 mkdir -p dist
-for whl in build/dist/*.whl; do
+for whl in rawdist/*.whl; do
     auditwheel repair "$whl" -w dist/
 done
-cp build/dist/*.tar.gz dist/
