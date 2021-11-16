ee1de328c4da04084bd6b33eb208ba9d171185ea
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-16 16:03:23 +0100

    Reuse started build if rerunning CI build scripts
    Useful when using these scripts to debug locally.
    
    See e.g.: MethodicalAcceleratorDesign/MAD-X#1033.

--- .github/build/manylinux1/madx.sh
+++ .github/build/manylinux1/madx.sh
@@ -5,23 +5,25 @@ set -ex
 # Must be run from the root the directory of the MAD-X sources.
 # Builds in './build' and installs to './dist'.
 
-rm -rf build
-mkdir build
+mkdir -p build
 cd build
 
 : ${PY:=/opt/python/cp36-cp36m/bin}
 
-$PY/pip install --upgrade cmake
-$PY/cmake .. \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DMADX_STATIC=ON \
-    -DCMAKE_INSTALL_PREFIX=../dist \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-fvisibility=hidden" \
-    -DCMAKE_CXX_FLAGS="-fvisibility=hidden" \
-    -DCMAKE_Fortran_FLAGS="-fvisibility=hidden" \
-    -DMADX_INSTALL_DOC=OFF \
-    -DMADX_ONLINE=OFF \
-    -DMADX_FORCE_32=OFF \
-    -DMADX_X11=OFF
+if [[ ! -f CMakeCache.txt ]]; then
+    $PY/pip install --upgrade cmake
+    $PY/cmake .. \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DMADX_STATIC=ON \
+        -DCMAKE_INSTALL_PREFIX=../dist \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_FLAGS="-fvisibility=hidden" \
+        -DCMAKE_CXX_FLAGS="-fvisibility=hidden" \
+        -DCMAKE_Fortran_FLAGS="-fvisibility=hidden" \
+        -DMADX_INSTALL_DOC=OFF \
+        -DMADX_ONLINE=OFF \
+        -DMADX_FORCE_32=OFF \
+        -DMADX_X11=OFF
+fi
+
 $PY/cmake --build . --target install -j
--- .github/build/msys2/madx.sh
+++ .github/build/msys2/madx.sh
@@ -5,20 +5,22 @@ set -ex
 # Must be run from the root the directory of the MAD-X sources.
 # Builds in './build' and installs to './dist'.
 
-rm -rf build
 mkdir build
 cd build
 
-# Build MAD-X as library:
-pip install --upgrade cmake
-cmake .. \
-    -G "MinGW Makefiles" \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DMADX_STATIC=ON \
-    -DCMAKE_INSTALL_PREFIX=../dist \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DMADX_INSTALL_DOC=OFF \
-    -DMADX_ONLINE=OFF \
-    -DMADX_FORCE_32=OFF \
-    -DMADX_X11=OFF
+if [[ ! -f CMakeCache.txt ]]; then
+    # Build MAD-X as library:
+    pip install --upgrade cmake
+    cmake .. \
+        -G "MinGW Makefiles" \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DMADX_STATIC=ON \
+        -DCMAKE_INSTALL_PREFIX=../dist \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DMADX_INSTALL_DOC=OFF \
+        -DMADX_ONLINE=OFF \
+        -DMADX_FORCE_32=OFF \
+        -DMADX_X11=OFF
+fi
+
 cmake --build . --target install -j
