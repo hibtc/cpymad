c101bffc45f9c2be4bc4123caeec5143b7ce1c71
Thomas Gläßle <t_glaessle@gmx.de>
2023-01-24 15:08:18 +0100

    Drop dependency on rediraffe

--- doc/conf.py
+++ doc/conf.py
@@ -5,7 +5,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinxext.rediraffe',
     'sphinx_substitution_extensions',
     'sphinx_automodapi.automodapi',
     'sphinx_automodapi.smart_resolver',     # see sphinx-autodoc-typehints#38
@@ -30,11 +29,6 @@ intersphinx_mapping = {
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
     'python': ('https://docs.python.org/3', None),
-}
-
-# Redirect:
-rediraffe_redirects = {
-    "installation/unix.rst": "installation/linux.rst",
 }
 
 # General information about the project.
@@ -73,6 +67,7 @@ htmlhelp_basename = 'cpymaddoc'
 # -- Fix broken links to non-existing source for auto-generated pages -----
 def setup(app):
     app.connect("html-page-context", html_page_context)
+    app.connect("build-finished", build_redirects)
 
 
 def html_page_context(app, pagename, templatename, context, doctree):
@@ -80,3 +75,34 @@ def html_page_context(app, pagename, templatename, context, doctree):
     if pagename.startswith('automod/'):
         context['meta'] = None
         context['display_github'] = False
+
+# Redirects
+
+def build_redirects(app, exception):
+    redirect(app, 'installation/unix.html', 'installation/linux.html')
+
+
+def redirect(app, src, dst):
+    # Lightweight alternative to 'sphinxext-rediraffe'
+    import os
+    dst = os.path.relpath(dst, os.path.dirname(src))
+    src = os.path.join(app.outdir, src)
+    os.makedirs(os.path.dirname(src), exist_ok=True)
+    with open(src, 'wt') as f:
+        f.write(f"""
+<html>
+    <head>
+        <noscript>
+            <meta http-equiv="refresh" content="0; url={dst}"/>
+        </noscript>
+    </head>
+    <body>
+        <script>
+            window.location.href = '{dst}' + (window.location.search || '') + (window.location.hash || '');
+        </script>
+        <p>This page has moved.</p>
+        <p>If you are not getting redirected please
+        <a href="{dst}">click here to continue.</a></p>
+    </body>
+</html>
+""")
