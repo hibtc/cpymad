f2995a614cccfdcf2432d5387ffa8bcf5c48bd12
Thomas Gläßle <t_glaessle@gmx.de>
2023-01-25 19:14:48 +0100

    Specify more compiler tools in MacOS build

--- .github/build/macos/setup_compiler.sh
+++ .github/build/macos/setup_compiler.sh
@@ -1,9 +1,13 @@
 # gcc-12 is the latest compiler version that includes gfortran that I could
 # find on the MacOS runner. This may change, so be careful:
 export CC=gcc-12
+export CPP=cpp-12
 export CXX=g++-12
 export FC=gfortran-12
+export LD=gcc-12
 
 which "$CC"
+which "$CPP"
 which "$CXX"
 which "$FC"
+which "$LD"
