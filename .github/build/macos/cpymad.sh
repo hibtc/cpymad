84ca8e0795bdab7ff36217c1a944208b2e513d14
Thomas Gläßle <t_glaessle@gmx.de>
2023-02-02 14:58:57 +0100

    Replace conda by setup-python in MacOS build
    This was needed to get the python 3.6 wheel delocated.
    
    Also, although adding a bit of boilerplate code it does slightly reduce
    overall complexity and runner time.

--- .github/build/macos/cpymad.sh
+++ .github/build/macos/cpymad.sh
@@ -1,23 +1,18 @@
 #! /usr/bin/env bash
 # Build cpymad from checked out sources.
 #
-# Usage: cpymad.sh <MADXDIR> <PYVER>
+# Usage: cpymad.sh <MADXDIR>
 #
 # Arguments:
 #   <MADXDIR>: MAD-X installation directory
-#   <PYVER>:   python version (e.g. "3.9")
 #
 # Outputs:
 #   ./build: builds here
 #   ./dist:  places wheels here
 set -e
 export MADXDIR=$1
-export PYVER=$2
 export BLAS=1 LAPACK=1
 
-conda create -qyf -n py$PYVER python=$PYVER -c anaconda
-conda activate py$PYVER
 pip install -U setuptools wheel cython
 rm -f src/cpymad/libmadx.c
 python setup.py sdist bdist_wheel
-conda deactivate
